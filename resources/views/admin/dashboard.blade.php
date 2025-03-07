c16eedd1dcb4270d0aca81ff04f2326e50bdb07a
LansDoank <maulana09180@gmail.com>
2025-03-07 15:44:21 +0700

    mematikan app debug

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -361,7 +361,6 @@
     <!-- Page level plugins -->
     <script src="/vendor/chart.js/Chart.min.js"></script>
 
-    <!-- Page level custom scripts -->
     <script src="/js/demo/chart-area-demo.js"></script>
     <script src="/js/demo/chart-pie-demo.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
